33edf5593bf7f46b30a30ebb7da06369c33676f8
MaxLenk <maxlenk@gmail.com>
2017-09-18 18:18:22 -0400

    Update Pd_geometry_optimization.py

--- Pd_geometry_optimization.py
+++ Pd_geometry_optimization.py
@@ -5,12 +5,15 @@ from ase.optimize import QuasiNewton
 #f = open('WCl_adsorption/W_geometry_optimization_500_441/energy.txt', 'w')
 #f.write('starting')  # python will convert \n to os.linesep
 
-W_slab=read('WCl_adsorption/W_111.traj')
-W_slab.calc=espresso(pw=450,
+Pd_slab_path = 'Pd_111.traj'
+output_directory = 'Pd_geometry_optimization_450_571/'
+
+Pd_slab=read('Pd_slab_path')
+Pd_slab.calc=espresso(pw=450,
                        dw=4500,
                        kpts=(5,7,1),
                        xc='PBE',
-                       outdir='WCl_adsorption/W_geometry_optimization_450_571/E_W_slab',#espresso outdirectory saved
+                       outdir= output_directory + 'E_Pd_slab',#espresso outdirectory saved
                                             #here                                    
                        convergence={'energy':1e-6,
                                     'mixing':0.05,
@@ -18,14 +21,14 @@ W_slab.calc=espresso(pw=450,
                                     'maxsteps':1000,
                                     'diag':'cg'})
 
-relax_W_slab=QuasiNewton(W_slab,
-                           logfile='WCl_adsorption/W_geometry_optimization_450_571/opt.log',
-                           trajectory='WCl_adsorption/W_geometry_optimization_450_571/opt.traj',
-                           restart='WCl_adsorption/W_geometry_optimization_450_571/opt.pckl') #ase output
-relax_W_slab.run(fmax=0.05)
+relax_Pd_slab=QuasiNewton(Pd_slab,
+                           logfile=output_directory + 'opt.log',
+                           trajectory=output_directory + 'opt.traj',
+                           restart=output_directory + 'opt.pckl') #ase output
+relax_Pd_slab.run(fmax=0.05)
 
-E_W_slab=W_slab.get_potential_energy()
+E_Pd_slab=Pd_slab.get_potential_energy()
 #f.write(E_W_slab)
 #f.write('\n END')
 #f.close()  # you can omit in most cases as the destructor will call it
-print(E_W_slab)
+print(E_Pd_slab)
